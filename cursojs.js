ab1d7366dd6820651b7a9a06ee21e4e16434447a
Ruben Correa <rcorrea@derecho.uba.ar>
2021-10-13 09:01:58 -0300

    primer comit de prueba

---
+++ cursojs.js
@@ -0,0 +1,17 @@
+/////////////////////Manejo de numeros///////////////////////
+
+/*var a = "10.55";
+document.write(typeof a)+"<br>"; ///devuelve string,number,boolean
+
+var a =10.55; // o "10.55"
+document.write(parseInt(a))+"<br>";//devuelve el numero entero 10
+*/
+
+/*var a ="10.55"; // o "10.55"
+document.write(parseFloat(a))+"<br>";//devuelve el numero entero 10
+document.write(typeof (parseFloat(a)) )// devuelve number
+*/
+
+var a = "hola";
+var b = isNaN(a);
+document.write(b);
